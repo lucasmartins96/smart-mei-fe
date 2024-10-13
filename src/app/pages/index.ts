4ab482ed43f9f2285b1aa679d6bd8be07f8219f1
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2024-10-13 12:01:36 -0300

    arquivo de importação de páginas

---
+++ src/app/pages/index.ts
@@ -0,0 +1,5 @@
+import { AuthComponent } from './auth/auth.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { BusinessPersonRegisterComponent } from './business-person-register/business-person-register.component';
+
+export { AuthComponent, WelcomeComponent, BusinessPersonRegisterComponent };
