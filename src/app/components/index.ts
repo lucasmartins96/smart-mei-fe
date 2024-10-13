b79c4e7c1c10dbd9c57d230c6e038c2bef72b9d0
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2024-10-13 11:59:21 -0300

    arquivo de importação de componentes

---
+++ src/app/components/index.ts
@@ -0,0 +1,5 @@
+import * as Forms from './forms/index';
+import { TabsSigninSignupComponent } from './tabs-signin-signup/tabs-signin-signup.component';
+import { DialogDefaultComponent } from './dialogs/dialog-default/dialog-default.component';
+
+export { Forms, TabsSigninSignupComponent, DialogDefaultComponent };
