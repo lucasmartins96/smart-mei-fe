9e068586aa4ca4644de36ffe0e0b23bb4bc5c467
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2024-10-13 12:02:59 -0300

    configuração das rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AuthComponent, WelcomeComponent } from './pages/index';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'auth', component: AuthComponent },
+  { path: '', redirectTo: '/auth', pathMatch: 'full' },
+];
