44f3b0746ba4b42ad2a92b8335711de59a341d26
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2024-11-02 11:10:29 -0300

    novas rotas em app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,26 @@
 import { Routes } from '@angular/router';
-import { AuthComponent, WelcomeComponent } from './pages/index';
+import {
+  AuthComponent,
+  BusinessPersonRegisterComponent,
+  CompanyRegisterComponent,
+  HomeComponent,
+  WelcomeComponent,
+} from './pages/index';
 
 export const routes: Routes = [
   { path: 'welcome', component: WelcomeComponent },
   { path: 'auth', component: AuthComponent },
+  {
+    path: 'business-person-register',
+    component: BusinessPersonRegisterComponent,
+  },
+  {
+    path: 'company-register',
+    component: CompanyRegisterComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
   { path: '', redirectTo: '/auth', pathMatch: 'full' },
 ];
