c37ba28b9f116ec963af60347ac1bf81ea82d0ef
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2024-11-03 13:06:45 -0300

    layout inicial da página home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
-  imports: [],
+  imports: [
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+  ],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  linkList = [
+    {
+      label: 'Dashboard',
+      routerLink: 'dashboard',
+    },
+    {
+      label: 'Movimentações',
+      routerLink: 'transactions',
+    },
+    {
+      label: 'Relatórios',
+      routerLink: 'reports',
+    },
+  ];
+}
