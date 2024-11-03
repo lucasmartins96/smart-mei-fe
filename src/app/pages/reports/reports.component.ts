65e8f66eb626eda7bb3377ad8fb3673fa8e95a1b
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2024-11-03 13:03:33 -0300

    novos componentes de páginas
    - app-dashboard
    - app-reports
    - app-transactions

---
+++ src/app/pages/reports/reports.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reports',
+  standalone: true,
+  templateUrl: './reports.component.html',
+  styleUrl: './reports.component.scss',
+  imports: [],
+})
+export class ReportsComponent {}
